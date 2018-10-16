8a1dc6ee0e6bee53a26d127bcd25769da21cd32f
Wellington Alves <wellsu@gmail.com>
2018-10-16 17:21:32 -0300

    Projeto criado

---
+++ src/main/java/com/alves/cursomc/CursomscApplication.java
@@ -0,0 +1,12 @@
+package com.alves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomscApplication.class, args);
+	}
+}
